46b5250da3ab211a40dfcc5dabe3e7af1611860a
sandy.kim <sandy.kim@cheilpengtai.com>
2023-08-09 15:49:37 +0900

    json 파싱 컨트롤러 추가, 프로퍼티 추가

--- src/main/java/com/example/cms/domain/member/Member.java
+++ src/main/java/com/example/cms/domain/member/Member.java
@@ -1,4 +1,33 @@
 package com.example.cms.domain.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+
+@ToString
+@NoArgsConstructor
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String phone;
+
+    public Member(Long id, String name, Integer age, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+    }
 }
