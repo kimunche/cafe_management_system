8e50c5b32e96c187118fcd853c9f063172de5ae4
sandy.kim <sandy.kim@cheilpengtai.com>
2023-08-21 17:02:23 +0900

    [ISSUE-4-1] feat: item create 시에 중복체크 추가

--- src/main/java/com/example/cms/item/repository/ItemRepository.java
+++ src/main/java/com/example/cms/item/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.example.cms.item.repository;
 
 import com.example.cms.item.domain.Item;
+import com.example.cms.item.domain.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +11,15 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAll();
-    Optional<Item> findByNameLike(String name);
+    //Optional<Item> findAllByNameLike(String name);
 
-    boolean existsByName(String name);
+    List<Item> findAllByNameContaining(String name);
 
-    void deleteByItemId(String name);
+    boolean existsByNameAndAndHotIce(String name, ItemStatus hotIce);
+
+    Optional<Item> findByNameAndHotIce(String name, ItemStatus hotIce);
+
+    void deleteByItemId(Long id);
+
+    boolean findByNameAndCostAndHotIce(String name, int cost, ItemStatus hotIce);
 }
