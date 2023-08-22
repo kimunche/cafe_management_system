84ef178821d359d6eabc877dd2a2ce3696252dbc
sandy.kim <sandy.kim@cheilpengtai.com>
2023-08-22 16:56:06 +0900

    [ISSUE-4-1] fix: item response 수정

--- src/main/java/com/example/cms/item/controller/response/ItemResponse.java
+++ src/main/java/com/example/cms/item/controller/response/ItemResponse.java
@@ -1,6 +1,7 @@
 package com.example.cms.item.controller.response;
 
 import com.example.cms.item.domain.Item;
+import com.example.cms.item.domain.ItemStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,17 +11,20 @@ import lombok.NoArgsConstructor;
 public class ItemResponse {
     private String name;
     private Integer cost;
+    private ItemStatus hotIce;
 
     @Builder
-    public ItemResponse(String name, Integer cost) {
+    public ItemResponse(String name, Integer cost, ItemStatus hotIce) {
         this.name = name;
         this.cost = cost;
+        this.hotIce = hotIce;
     }
 
     public static ItemResponse of(Item item){
         return ItemResponse.builder()
                 .name(item.getName())
                 .cost(item.getCost())
+                .hotIce(item.getHotIce())
                 .build();
     }
 }
