e857426ca19620feeabc9b48c86add9b7b126945
Mars <mars.kim@cheilpengtai.com>
2023-09-06 17:11:23 +0900

    [ISSUE-6] QueryDsl 수정
    수정후 커밋

--- src/main/java/com/example/cms/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/example/cms/member/repository/MemberRepositoryImpl.java
@@ -1,13 +1,15 @@
 package com.example.cms.member.repository;
 
 import com.example.cms.member.domain.Member;
+
 import com.example.cms.member.domain.QMember;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-
+@Transactional
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
@@ -17,11 +19,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     }
 
     public List<Member> findMemberList(){
-
-        List<Tuple> fetch = queryFactory.select().from(QMember.member).fetch();
-
-
-        return null;
+        return queryFactory.selectFrom(QMember.member).fetch();
     }
 
 }
